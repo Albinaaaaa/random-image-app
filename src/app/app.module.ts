da7dfd0b05e8bd933151052f0462f56aea490c99
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-07-14 22:24:38 +0300

    add handler fro show image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShowImageComponent } from './components/show-image/show-image.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ShowImageComponent } from './components/show-image/show-image.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/show-image/show-image.component.ts
+++ src/app/components/show-image/show-image.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Icons from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-show-image',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./show-image.component.scss']
 })
 export class ShowImageComponent {
+  iconsNames: string[] = Object.keys(Icons);
+  iconProp = Icons.fa0;
+  hidden = true;
 
+
+  ngOnInit() {}
+
+  constructor() {}
+
+  showRandomImage() {
+    setTimeout(() => {
+      if (this.hidden) {
+        this.hidden = false;
+      }
+      const randomNumber = Math.floor(Math.random() * this.iconsNames.length);
+      const iconName = this.iconsNames[randomNumber];
+      this.iconProp = Icons[iconName];
+    }, 3000);
+  }
 }
+
