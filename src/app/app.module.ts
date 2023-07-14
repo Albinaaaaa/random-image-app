e93ba1f91ee930d7798e4f7df5dcbd7485156dc3
aaalbinaaaa <albinarubanchuk@gmail.com>
2023-07-14 21:26:14 +0300

    add show-image component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowImageComponent } from './components/show-image/show-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowImageComponent
   ],
   imports: [
     BrowserModule,
